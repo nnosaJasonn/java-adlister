c190e0c9f0acbab4944e0e8bae70b4af37e35451
lindsey.jason.andrew@gmail.com <jasonlindsey@jasons-mbp.attlocal.net>
2019-06-12 22:04:06 -0500

    here we go again on my own

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -17,6 +17,7 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            request.getSession().setAttribute("isAdmin", true);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
